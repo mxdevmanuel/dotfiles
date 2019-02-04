bd4ce9fe72bcbc0535124c4c28ad5b0725ffa6ec
Manuel Morales <p513manu@gmail.com>
2019-02-04 08:44:59 -0600

    switch to arch

--- conky/.config/conky/outlook.sh
+++ conky/.config/conky/outlook.sh
@@ -2,4 +2,10 @@
 FETCH=$(fetchmail --check)
 TOTAL=$(echo "${FETCH}" | cut -d" " -f1)
 READ=$(echo "${FETCH}" | cut -d" " -f3 | sed 's/(//')
-echo $(echo "$TOTAL - $READ" | bc)
+
+if [[ $READ =~ '^[0-9]+$' ]]
+then
+	echo $(echo "$TOTAL - $READ" | bc)
+else
+	echo $TOTAL
+fi
--- power_management/check_battery.sh
+++ power_management/check_battery.sh
@@ -7,14 +7,13 @@ ACPI=$(acpi)
 STATUS=$(echo $ACPI | grep 'Discharging')
 BATTERY_LEVEL=$(echo $ACPI | awk '{ print $4 } ' | sed 's/[^[:digit:]]//g' | bc)
 
-BATTERY_DANGER=$(echo $BATTERY_LEVEL | awk '{print $1 < 5}' | bc)
+BATTERY_DANGER=$(echo $BATTERY_LEVEL | awk '{print $1 < 100}' | bc)
 
 if [[ -n "${STATUS}" ]]
 then
 	if [[ "$BATTERY_DANGER" -ne "0"  ]]
 	then
-		notify-send -u critical -i "/usr/share/icons/Paper/16x16/status/xfce-battery-critical.png" -t 3000 "Hibernando el sistema"
-		sudo systemctl hibernate
+		notify-send -u critical -i "/usr/share/icons/Paper/16x16/status/xfce-battery-critical.png" -t 3000 "Hibernando el sistema"; sudo systemctl -i hibernate
 	elif [[ "$BATTERY_LEVEL" -le "10" ]]
 	then
 		notify-send -u critical -i "/usr/share/icons/Paper/16x16/status/xfce-battery-critical.png" -t 3000 $(echo $ACPI | awk '{ print $5 " " $6 }')
