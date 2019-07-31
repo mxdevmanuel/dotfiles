87c9ef2791bced6b3c0d064cf51a36a3e3416c14
Manuel Morales <p513manu@gmail.com>
2019-07-31 12:17:30 -0500

    before branch

--- i3/.config/i3/moc_mode.sh
+++ i3/.config/i3/moc_mode.sh
@@ -4,6 +4,6 @@ if [ -z $running ]
 then
 	termite -e "mocp -m"
 else
-	while sleep 1; do echo $(mocp -i | grep Title); done | dzen2 -w 1920
+	while sleep 1; do echo $(mocp -i | grep -i ^Title); done | dzen2 -w 1920
 fi
 
