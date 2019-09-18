a9bbe321e0a82a2c11c805ea5404be74ed1628a5
Manuel Morales <p513manu@gmail.com>
2019-09-17 19:19:42 -0500

    f*ck

--- i3/.config/i3/unsplash.sh
+++ i3/.config/i3/unsplash.sh
@@ -2,7 +2,7 @@ LAND=~/Imágenes/Wallpapers/Unsplash/$(date -d "today" +"%Y%m%d").jpg
 wget "https://source.unsplash.com/featured/1920x1080/daily/?landscape" -O $LAND
 
 ARCH=~/Imágenes/Wallpapers/Architecture/$(date -d "today" +"%Y%m%d").jpg
-wget "https://source.unsplash.com/featured/1921x1080/daily/?architecture" -O $ARCH
+wget "https://source.unsplash.com/featured/1920x1080/daily/?architecture" -O $ARCH
 DOW=$(date +%u)
 
 if (( $DOW % 2 ))
