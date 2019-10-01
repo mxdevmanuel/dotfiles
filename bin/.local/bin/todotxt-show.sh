b21f1c905fba0d55e8302eaebf8bb0924f788d1c
Manuel Morales <p513manu@gmail.com>
2019-09-30 20:06:56 -0500

    added markdone

--- bin/.local/bin/todotxt-show.sh
+++ bin/.local/bin/todotxt-show.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 OUTPUT=$(grep -v "^x"  ~/Todo/todo.txt | sort | rofi -dmenu -p tasks)
-if [ ! -z "$OUTPUT" ]
+
+if [ $? ==  10 ]
+then
+        echo $OUTPUT | todotxt-markdone.sh
+elif [ ! -z "$OUTPUT" ]
 then
         termite -e "zsh -c \"vim /home/$USER/Todo/todo.txt &&  notify-send Todo.txt 'Remeber to sync the todo file' --icon='/usr/share/icons/Papirus/48x48/apps/gnome-todo.svg' -t 3000\"" &
         disown %1
--- bin/.local/bin/todotxt-sync.sh
+++ bin/.local/bin/todotxt-sync.sh
@@ -5,11 +5,13 @@ case $OPTION in
         "copy")
                 echo "copy"
                 rclone sync drive:/todo ~/Todo
+                notify-send Todo.txt 'Local todo.txt updated' --icon='/usr/share/icons/Papirus/48x48/apps/gnome-todo.svg' -t 3000\
                 ;;
                 
         "update")
                 echo "update"
                 rclone sync ~/Todo drive:/todo
+                notify-send Todo.txt 'Remote todo.txt synced' --icon='/usr/share/icons/Papirus/48x48/apps/gnome-todo.svg' -t 3000\
                 ;;
         "edit")
                 echo "edit"
