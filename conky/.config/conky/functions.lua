83d532bb25c2b701628c04be7cc824cc82857ccb
Manuel Morales <p513manu@gmail.com>
2020-06-03 14:33:41 -0500

    fix new line in lua script

--- conky/.config/conky/functions.lua
+++ conky/.config/conky/functions.lua
@@ -1,11 +1,12 @@
 
 function conky_variable_processors()
-        first_second_core = [[${color}${font} CPU1: ${cpu cpu1}% ${freq_g 1}GHz  CPU2: ${cpu cpu2}% ${freq_g 2}GHz]]
+        first_second_core = "${color}${font} CPU1: ${cpu cpu1}% ${freq_g 1}GHz  CPU2: ${cpu cpu2}% ${freq_g 2}GHz"
 
-        third_fourth_core = [[${color}${font} CPU3: ${cpu cpu3}% ${freq_g 3}GHz  CPU4: ${cpu cpu4}% ${freq_g 4}GHz]]
+        third_fourth_core = "${color}${font} CPU3: ${cpu cpu3}% ${freq_g 3}GHz  CPU4: ${cpu cpu4}% ${freq_g 4}GHz"
+
         menv = os.getenv('MACHINE_ENV')
         if menv == 'desktop' then
-                return first_second_core .. third_fourth_core
+                return first_second_core .. "\n" .. third_fourth_core
         else
                 return first_second_core
         end
