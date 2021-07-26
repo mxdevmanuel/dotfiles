39f687e513209d6bffd6d2023ed3cd59850130a0
Manuel Morales <p513manu@gmail.com>
2021-07-25 23:15:12 -0500

    procrastination: on

--- bin/.local/bin/lolarch.zsh
+++ bin/.local/bin/lolarch.zsh
@@ -4,11 +4,17 @@ clear
 
 local LOLPATH=$HOME/Envs/tests/bin
 
+local termcolors=""
+for i in $(seq 1 16)
+do
+	 termcolors+=$( echo -en `tput setab $i` '   ' `tput sgr0` )
+	 [[ $i == 8 ]] && termcolors+="\n"
+done
+
 if [[ -x $LOLPATH/lolcat ]]
 then
-	paste -d '' <(cat $HOME/.ascii/arch.txt | $LOLPATH/lolcat ) <(neofetch --stdout | sed "s,:.*,$(tput sgr0)&,")
+	paste -d '\ ' <(cat $HOME/.ascii/arch.txt | $LOLPATH/lolcat ) <(neofetch --stdout | sed -e "s,:.*,$(tput sgr0)&," -e '$a'"$termcolors") | tr -d "%"
 else
 	neofetch
 fi
 
-echo -ne '\n\t' ; for i in $(seq 1 16); do echo -en `tput setab $i` '   ' `tput sgr0`; done; echo -e ''
