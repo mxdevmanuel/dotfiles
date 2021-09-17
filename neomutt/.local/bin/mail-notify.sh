e8aff2a3050c7caed58b9b19dc19b34bda17c90e
Manuel Morales <p513manu@gmail.com>
2021-09-17 15:06:58 -0500

    feat: uppercase mailbox names on notification

--- neomutt/.local/bin/mail-notify.sh
+++ neomutt/.local/bin/mail-notify.sh
@@ -13,9 +13,14 @@ _notify(){
 	[[ -z "$inboxes" ]] && exit 0
 
 	local news=`eval "find ${inboxes} -type f -newer ${TIMEFILE}" \
-		| awk 'BEGIN { FS = "/" } ; { print $2 }' | sort | uniq -c`
+		| awk 'BEGIN { FS = "/" } ; { print toupper( substr( $2, 1, 1 ) ) substr( $2, 2 ); }' \
+		| sort | uniq -c`
 	
-	[[ ! -z "$news" ]] && notify-send --icon=$HOME/.local/share/icons/Gruvbox-Material-Dark/64x64/apps/email.svg 'New Email' "${news}" -t 10000 
+	if [[ ! -z "$news" ]] 
+	then
+		notify-send --icon=$HOME/.local/share/icons/Gruvbox-Material-Dark/64x64/apps/email.svg\
+			'New Email' "${news}" -t 10000 
+	fi
 
 	touch $TIMEFILE
 }
