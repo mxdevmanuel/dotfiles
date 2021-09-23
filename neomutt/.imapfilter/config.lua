7088f1bb9582338dde96cbf4e6b36acfb9884a4a
Manuel Morales <p513manu@gmail.com>
2021-09-23 16:39:10 -0500

    feat: added imapfilter before mbsync

--- neomutt/.imapfilter/config.lua
+++ neomutt/.imapfilter/config.lua
@@ -1,10 +1,11 @@
-local inspect = require('inspect')
-
+-- Options
 options.create = true
 options.certificates = true
 options.starttls = true
 options.close = true
 options.timeout = 60
+
+-- Functions
 
 function pass(account)
     local command = "pass " .. account
@@ -17,6 +18,7 @@ function pass(account)
     return result
 end
 
+-- Gmail
 gmailuser = pass('gmail/user')
 gmailpass = pass('gmail/password')
 
@@ -35,3 +37,27 @@ print(gmailinbox:check_status())
 clientesbbva = gmailinbox:contain_from("clientes@bbva.mx")
 
 clientesbbva:move_messages(gmailaccount["BBVA"])
+
+-- Outlook
+outlookuser = pass('outlook/user')
+outlookpass = pass('outlook/password')
+
+outlookaccount = IMAP {
+    server = 'outlook.office365.com',
+    port = 993,
+    username = outlookuser,
+    password = outlookpass,
+    ssl = 'auto'
+}
+
+outlookinbox = outlookaccount["INBOX"]
+
+print(outlookinbox:check_status())
+
+zoom = outlookinbox:contain_from("no-reply@zoom.us")
+
+zoom:delete_messages()
+
+linkedin = outlookinbox:contain_from("updates-noreply@linkedin.com")
+
+linkedin:delete_messages()
